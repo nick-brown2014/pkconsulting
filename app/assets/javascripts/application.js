173e1be9cc3eb0b779ae3577cada6b2e502b5253
Nick Brown <nick.brown2014@gmail.com>
2015-04-28 16:06:06 -0500

    added some slick javascript to landing page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,4 +17,106 @@
 
 $(document).ready(function(){
 
+  $("#logo_one").click(function(event) {
+    event.preventDefault();
+    var height = $("#row_one").height();
+    $("#row_one").fadeOut("slow", function() {
+    $("#banner_one").height(height);
+    $("#banner_one").fadeIn("slow");
+    });
+  });
+
+  $("#logo_two").click(function(event) {
+    event.preventDefault();
+    var height = $("#row_one").height();
+    $("#row_one").fadeOut("slow", function() {
+    $("#banner_one").height(height);
+    $("#banner_one").fadeIn("slow");
+    });
+  });
+
+  $("#logo_three").click(function(event) {
+    event.preventDefault();
+    var height = $("#row_one").height();
+    $("#row_one").fadeOut("slow", function() {
+    $("#banner_one").height(height);
+    $("#banner_one").fadeIn("slow");
+    });
+  });
+
+  $("#logo_four").click(function(event) {
+    event.preventDefault();
+    var height = $("#row_two").height();
+    $("#row_two").fadeOut("slow", function() {
+    $("#banner_two").height(height);
+    $("#banner_two").fadeIn("slow");
+    });
+  });
+
+  $("#logo_five").click(function(event) {
+    event.preventDefault();
+    var height = $("#row_two").height();
+    $("#row_two").fadeOut("slow", function() {
+    $("#banner_two").height(height);
+    $("#banner_two").fadeIn("slow");
+    });
+  });
+
+  $("#logo_six").click(function(event) {
+    event.preventDefault();
+    var height = $("#row_two").height();
+    $("#row_two").fadeOut("slow", function() {
+    $("#banner_two").height(height);
+    $("#banner_two").fadeIn("slow");
+    });
+  });
+
+  $("#logo_seven").click(function(event) {
+    event.preventDefault();
+    var height = $("#row_three").height();
+    $("#row_three").fadeOut("slow", function() {
+    $("#banner_three").height(height);
+    $("#banner_three").fadeIn("slow");
+    });
+  });
+
+  $("#logo_eight").click(function(event) {
+    event.preventDefault();
+    var height = $("#row_three").height();
+    $("#row_three").fadeOut("slow", function() {
+    $("#banner_three").height(height);
+    $("#banner_three").fadeIn("slow");
+    });
+  });
+
+  $("#logo_nine").click(function(event) {
+    event.preventDefault();
+    var height = $("#row_three").height();
+    $("#row_three").fadeOut("slow", function() {
+    $("#banner_three").height(height);
+    $("#banner_three").fadeIn("slow");
+    });
+  });
+
+  $("#banner_one").click(function(event) {
+    event.preventDefault();
+    $("#banner_one").fadeOut("slow", function() {
+    $("#row_one").fadeIn("slow");
+    });
+  });
+
+  $("#banner_two").click(function(event) {
+    event.preventDefault();
+    $("#banner_two").fadeOut("slow", function() {
+    $("#row_two").fadeIn("slow");
+    });
+  });
+
+  $("#banner_three").click(function(event) {
+    event.preventDefault();
+    $("#banner_three").fadeOut("slow", function() {
+    $("#row_three").fadeIn("slow");
+    });
+  });
+
 })
