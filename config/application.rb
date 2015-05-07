f69ae2b94b24dbb120bd10a632276c382e27a87d
Nick Brown <nick.brown2014@gmail.com>
2015-05-07 16:01:46 -0500

    fixing precompile in heroku error

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,6 @@ require "action_view/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
-config.serve_static_assets = true
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -18,6 +17,7 @@ Bundler.require(*Rails.groups)
 
 module Pkconsulting
   class Application < Rails::Application
+    config.serve_static_assets = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
